725694cea049f43c5eee4ab9f94fc329b9ec5bb5
Lu-Pereira <luci.lv14@gmail.com>
2020-10-26 16:45:49 -0300

    Implementação de funcionalidades

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
 const cipher = {
-  // ...
+
+   encode: (offset, text) => {
+    let cifrar = '';
+    for (let i = 0; i < text.length; i++) {
+      if(text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+        cifrar+=String.fromCharCode(((text.charCodeAt(i)-65+offset)%26)+65);
+      } if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+        cifrar+=String.fromCharCode(((text.charCodeAt(i)-97+offset)%26)+97);
+      } if (text.charCodeAt(i) >= 32 && text.charCodeAt(i) <= 64) {
+        cifrar+=String.fromCharCode(((text.charCodeAt(i)-32+offset)%33)+32);
+      }
+    }
+    return cifrar;
+  },
+
+   decode: (offset, text) => {
+     let decifrar = '';
+     for (let i = 0; i < text.length; i++) {
+       if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+         decifrar += String.fromCharCode(((text.charCodeAt(i) - 90 - offset) % 26) + 90);
+       } if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+         decifrar += String.fromCharCode(((text.charCodeAt(i) - 122 - offset) % 26) + 122);
+       } if (text.charCodeAt(i) >= 32 && text.charCodeAt(i) <= 64) {
+         decifrar += String.fromCharCode(((text.charCodeAt(i) - 64 - offset) % 33) + 64);
+       }
+     }
+     return decifrar;
+   }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+document.getElementById('encode').addEventListener('click', cifrar);
+    function cifrar() {
+    const text = document.getElementById("msg").value;
+    const offset = Number(document.getElementById("chave").value);
+    // let texto = cipher.encode(offset, text);
+    document.getElementById('text-encoded').innerHTML = cipher.encode(offset, text);
+
+    // `<p><stonge>${texto}</stonge></p>`;
+}
+
+document.getElementById('decode').addEventListener('click', decifrar);
+    function decifrar() {
+    const text = document.getElementById("msg").value;
+    const offset = Number(document.getElementById("chave").value);
+    // let texto = cipher.decode(offset, text);
+    document.getElementById('text-encoded').innerHTML = cipher.decode(offset, text);
+
+    // `<p><stonge>${texto}</stonge></p>`;
+}
