d0f9d5c774e2998e55192d211ccf0509301f809d
Lu-Pereira <luci.lv14@gmail.com>
2020-10-30 22:29:27 -0300

    implementando cipher e index e estilizando página

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,28 @@
 const cipher = {
 
-  encode: (offset, string) => {
+  encode: (offset, mensagem) => {
     let cifrar = '';
-    for (let i = 0; i < string.length; i++) {
-      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-        cifrar += String.fromCharCode(((string.charCodeAt(i) - 65 + offset) % 26) + 65);
-      } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-        cifrar += String.fromCharCode(((string.charCodeAt(i) - 97 + offset) % 26) + 97);
-      } else if (string.charCodeAt(i) >= 32 && string.charCodeAt(i) <= 64) {
-        cifrar += String.fromCharCode(((string.charCodeAt(i) - 32 + offset) % 33) + 32);
-      } 
+    for (let i = 0; i < mensagem.length; i++) {
+      if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
+        cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 65 + offset) % 26) + 65);
+      } else if (mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <= 122) {
+        cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 97 + offset) % 26) + 97);
+      } else if (mensagem.charCodeAt(i) >= 32 && mensagem.charCodeAt(i) <= 64) {
+        cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 32 + offset) % 33) + 32);
+      }
     }
     return cifrar;
   },
 
-  decode: (offset, string) => {
+  decode: (offset, mensagem) => {
     let decifrar = '';
-    for (let i = 0; i < string.length; i++) {
-      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-        decifrar += String.fromCharCode(((string.charCodeAt(i) - 90 - offset) % 26) + 90);
-      } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-        decifrar += String.fromCharCode(((string.charCodeAt(i) - 122 - offset) % 26) + 122);
-      } else if (string.charCodeAt(i) >= 32 && string.charCodeAt(i) <= 64) {
-        decifrar += String.fromCharCode(((string.charCodeAt(i) - 64 - offset) % 33) + 64);
+    for (let i = 0; i < mensagem.length; i++) {
+      if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
+        decifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 90 - offset) % 26) + 90);
+      } else if (mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <= 122) {
+        decifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 122 - offset) % 26) + 122);
+      } else if (mensagem.charCodeAt(i) >= 32 && mensagem.charCodeAt(i) <= 64) {
+        decifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 64 - offset) % 33) + 64);
       }
     }
     return decifrar;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 import cipher from './cipher.js';
 
 document.getElementById('encode').addEventListener('click', cifrar);
-    function cifrar() {
-    const string = document.getElementById("msg").value;
-    const offset = Number(document.getElementById("chave").value);
-    if (string == "" || offset == "" ) {
-      alert("Ops... você esqueceu de preencher o campo para CRIPTOGRAFAR a mensagem ou não informou a CHAVE, tente novamente.");
-      } else {
-      document.getElementById("encode_decode").innerHTML = cipher.encode(offset, string);
-    }
+function cifrar() {
+  const mensagem = document.getElementById("msg").value;
+  const offset = Number(document.getElementById("chave").value);
+  if (mensagem == "" || offset == "") {
+    alert("Ops... você esqueceu de preencher o campo para CRIPTOGRAFAR a mensagem ou não informou a CHAVE, tente novamente.");
+  } else {
+    document.getElementById("encode_decode").innerHTML = cipher.encode(offset, mensagem);
+  }
 }
 
 document.getElementById('decode').addEventListener('click', decifrar);
-    function decifrar() {
-    const string = document.getElementById("msg").value;
-    const offset = Number(document.getElementById("chave").value);
-    if (string == "" || offset == "" ) {
-      alert("Ops... você esqueceu de preencher o campo para DESCRIPTOGRAFAR a mensagem ou não informou a CHAVE, tente novamente.");
-      } else {
-      document.getElementById("encode_decode").innerHTML = cipher.decode(offset, string);
-    }
+function decifrar() {
+  const mensagem = document.getElementById("msg").value;
+  const offset = Number(document.getElementById("chave").value);
+  if (mensagem == "" || offset == "") {
+    alert("Ops... você esqueceu de preencher o campo para DESCRIPTOGRAFAR a mensagem ou não informou a CHAVE, tente novamente.");
+  } else {
+    document.getElementById("encode_decode").innerHTML = cipher.decode(offset, mensagem);
+  }
 }
+
+document.querySelector('h2', efeitoEscrita); //Para passar um classe ou tag
+// Efeito de escrita
+function efeitoEscrita(elemento) { //Passa o elemento escrita
+  const texto = elemento.innerHTML.split(''); //split dividira o texto em partes dentro da array
+  elemento.innerHTML = '';
+  texto.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra //Para adicionar as letras
+      , 75 * i)//Determinar o tempo para inserir o texto ou frase. Ou seja sera inserido cada letra a 75 milisegundos.
+  });
+}
+
+
