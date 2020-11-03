a043aa1ddd936a5961e95fc602d123eda5688447
Sara Viana <saravianac@outlook.com>
2020-11-03 10:50:36 -0300

    segundamudanca

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,5 @@ const cipher = {
 };
 
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+
+function sorteio() {
+    const getRandomIntegerInclusive = (min, max) =>
+        Math.floor(Math.random() * (max - min + 1)) + min;
+
+    document.getElementById("sorteio").innerHTML = sort;
+
+    console.log(getRandomIntegerInclusive(1, 100))
+}
+
+
