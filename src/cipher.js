e5eb061c467aff95c38c5007e2da446949b96092
Sara Viana <saravianac@outlook.com>
2020-11-05 10:12:36 -0300

    ultimaAtualizaçãoCOdigoFUncinando

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,38 @@
 const cipher = {
-  // ...
+  /*Criptografar*/
+  encode: function (offset, string) {
+    let palavraDigitada = "";
+    let maiusculo = string.toUpperCase();
+    for (let deslocamento = 0; deslocamento < maiusculo.length; deslocamento++) {
+      let codeLetra = maiusculo[deslocamento]
+      if (codeLetra !== " ") {
+        let novaLetra = codeLetra.charCodeAt();
+        let novoCode = ((novaLetra - 65 + parseInt(offset)) % 26) + 65;
+        codeLetra = String.fromCharCode(novoCode);
+      }
+      palavraDigitada = palavraDigitada + codeLetra;
+    }
+    return palavraDigitada
+  }/*Descriptografar*/
+  , decode: function (offset, string) {
+    let palavraDigitada = ""
+    let maiusculo = string.toUpperCase();
+    for (let deslocamento = 0; deslocamento < maiusculo.length; deslocamento++) {
+      let codeLetra = maiusculo[deslocamento]
+      if (codeLetra !== " ") {
+        let novaLetra = maiusculo[deslocamento].charCodeAt();
+        let novoCode = ((novaLetra + 65 - parseInt(offset)) % 26) + 65;
+        codeLetra = String.fromCharCode(novoCode);
+      }
+      palavraDigitada = palavraDigitada + codeLetra
+    }
+    return palavraDigitada
+
+  } /*Sorteio*/
+  , sorteio: function () {
+    return document.getElementById("resultado").innerHTML
+      = Math.floor(Math.random() * (25 - 1 + 1)) + 1;
+  }
 };
 
 export default cipher;
-
-
--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+let mensagemUsuario = document.querySelector("#msg")
+let offset = document.querySelector("#offset")
+let choice = document.querySelector("#choice")
+let choice2 = document.querySelector("#choice2")
+let result = document.querySelector("#result")
+let result_name = document.querySelector("#result-name")
+let btn_sorteio = document.querySelector("#btnSorteio");
 
 
-function sorteio() {
-    return document.getElementById("resultado").innerHTML = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 
+const showErrorEncode = () => {
+    if (mensagemUsuario.value == "" || offset.value == "") {
+        alert("CARO USUÁRIO, PREENCHA OS CAMPOS REQUERIDOS")
+    } else {
+        result_name.innerHTML = cipher.encode(offset.value, mensagemUsuario.value)
+        result.style.display = "block"
+    }
 }
-console.log("resultado")
+
+choice.addEventListener('click', showErrorEncode)
+
+const showErrorDecode = () => {
+    if (mensagemUsuario.value == "" || offset.value == "") {
+        alert("CARO USUÁRIO, PREENCHA OS CAMPOS REQUERIDOS")
+    } else {
+        result_name.innerHTML = cipher.decode(offset.value, mensagemUsuario.value)
+        result.style.display = "block"
+    }
+}
+
+choice2.addEventListener('click', showErrorDecode)
+
+btn_sorteio.addEventListener('click', cipher.sorteio);
