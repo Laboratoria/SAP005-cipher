20c82675d581fe0db8b4d5be1b29884e9254ab97
Talita-8 <tlthmf1@outlook.com>
2020-11-06 16:10:25 -0300

    Cifra finalizada

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,44 @@
-const cipher = {
-  // ...
-};
+ const cipher = {
 
-export default cipher;
+  encode: function (offset, string){
+  
+         const primeiraAsciiCode = 65
+         const tamanhoAlfabeto = 26
+         let unicodeString = ""
+         let codificado = ""
+         let auxiliarCode = ""
+
+      for(let i = 0; i < string.length; i++){
+        unicodeString = string.charCodeAt(i)
+        if(unicodeString == 32){
+          codificado += " "
+        }  else
+        auxiliarCode = ((unicodeString - primeiraAsciiCode + offset) % tamanhoAlfabeto) + primeiraAsciiCode
+        codificado += String.fromCharCode(auxiliarCode)     
+      }
+        return codificado
+  },
+
+  decode: function (offsetCipher, stringCipher){
+        const primeiraAsciiCode = 65
+        const tamanhoAlfabeto = 26
+        let unicodeCipher = ""
+        let descodificado = ""
+        let auxiliarCipher = ""
+    
+        for(let x = 0; x < stringCipher.length; x++){
+          unicodeCipher = stringCipher.charCodeAt(x)
+          console.log(unicodeCipher)
+          if(unicodeCipher == 32){
+            descodificado += " "
+          } else
+          auxiliarCipher = ((unicodeCipher + primeiraAsciiCode -offsetCipher) % tamanhoAlfabeto) + primeiraAsciiCode
+       //   if(auxiliarCode > 90)
+          descodificado += String.fromCharCode(auxiliarCipher)
+        }
+          return descodificado
+      }
+  
+}
+
+export default cipher;  
--- src/index.js
+++ src/index.js
@@ -1,5 +1,102 @@
 import cipher from './cipher.js';
-
 console.log(cipher);
+
+// Manipulação dos botões de opções do menu
+
+let menu1 = document.getElementById("option1")
+let menu2 = document.getElementById("option2")
+let menu3 = document.getElementById("option3")
+let menu4 = document.getElementById("option4")
+
+let botao1 = document.getElementById("click1")
+let botao2 = document.getElementById("click2")
+let botao3 = document.getElementById("click3")
+let botao4 = document.getElementById("click4")
+
+botao1.addEventListener("click", show1)
+botao2.addEventListener("click", show2)
+botao3.addEventListener("click", show3)
+botao4.addEventListener("click", show4)
+
+function show1(){
+ 
+   menu1.style.display = "block"
+   menu2.style.display = "none"
+   menu3.style.display = "none"
+   menu4.style.display = "none"
+ } 
+
+  function show2(){
+   menu1.style.display = "none"
+   menu2.style.display = "block"
+   menu3.style.display = "none"
+   menu4.style.display = "none"
+ }
+  function show3(){
+   menu1.style.display = "none"
+   menu2.style.display = "none"
+   menu3.style.display = "block"
+   menu4.style.display = "none"
+  
+ } 
+ function show4(){
+ 
+  menu1.style.display = "none"
+  menu2.style.display = "none"
+  menu3.style.display = "none"
+  menu4.style.display = "block"
+}
+           
+// Manipulação dos dados recebidos
+
+// Input e botão Criptofgrafar
+let inserirFrase = document.getElementById("capturar")
+inserirFrase.addEventListener("click", capturar)
+
+function capturar(event){
+  event.preventDefault()
+
+    let string = ""
+    let frase = document.getElementById("phrase").value
+    string = frase.toUpperCase()
+
+    // select do Offset
+    let obterDeslocamento = document.getElementById("offset").value
+    let offset = Number(obterDeslocamento)
+
+   //imprimir na tela
+    document.getElementById("valorFinal").innerHTML = cipher.encode(offset, string)
+}   
+
+// Input e botão Descriptografar
+
+let inserirCodigo = document.getElementById("capturarCipher")
+inserirCodigo.addEventListener("click", capturarCipher)
+
+function capturarCipher(event){
+  event.preventDefault()
+
+    let stringCipher = document.getElementById("phraseCipher").value
+
+    // select do Offset
+    let obterOffsetCipher = document.getElementById("offsetCipher").value
+    let offsetCipher = Number(obterOffsetCipher)
+
+   //imprimir na tela
+    document.getElementById("valorFinal").innerHTML = cipher.decode(offsetCipher, stringCipher)
+}   
+
+//botão de dica Opção 4
+
+let dica = document.getElementById("dica")
+dica.addEventListener("click", help)
+
+function help(event){
+  event.preventDefault()
+
+  let helpOption = document.getElementById("dica").innerHTML = "J = A"
+
+}
+
 
 
