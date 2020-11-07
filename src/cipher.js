df0a18c2c5cfbf21dad35c3ab04933d4863c3b0e
Lu-Pereira <luci.lv14@gmail.com>
2020-11-07 02:11:45 -0300

    atualização do cipher e estrutura html

--- src/cipher.js
+++ src/cipher.js
@@ -3,12 +3,12 @@ const cipher = {
 
   encode: (offset, mensagem) => {
     let cifrar = '';
-    if (mensagem == null || mensagem == "") {
-      throw new TypeError("E necessario inserir chave e texto", "cipher.js", 10); 
+    if (mensagem == null || mensagem == "" || offset == null || offset == "") { // Criação de um TypeError para a condição de null
+      throw new TypeError("E necessario inserir chave e texto, por favor revise os campos", "cipher.js");
     } else {
       for (let i = 0; i < mensagem.length; i++) {
         if ((mensagem.charCodeAt(i) >= 65) && (mensagem.charCodeAt(i) <= 90)) { // Do A ao Z, de acordo com a posição do ASCII.
-          cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 65 + offset) % 26) + 65);
+          cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 65 + offset) % 26) + 65); // Utilizando o String.fromCharCode para converter o  codigo ASCII
         } else if ((mensagem.charCodeAt(i) >= 97) && (mensagem.charCodeAt(i) <= 122)) { // Do A ao Z minusculo, de acordo com a posição do ASCII.
           cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 97 + offset) % 26) + 97);
         } else if ((mensagem.charCodeAt(i) >= 32) && (mensagem.charCodeAt(i) <= 64)) { // Do espaço, numeros e simbolos, de acordo com a posição do ASCII.
@@ -22,7 +22,7 @@ const cipher = {
   decode: (offset, mensagem) => {
     let decifrar = '';
     if (mensagem == null || mensagem == "" || offset == null || offset == "") {
-      throw new TypeError("E necessario inserir chave e texto","cipher.js", 10);
+      throw new TypeError("E necessario inserir chave e texto, por favor revise os campos", "cipher.js");
     } else {
       for (let i = 0; i < mensagem.length; i++) {
         if ((mensagem.charCodeAt(i) >= 65) && (mensagem.charCodeAt(i) <= 90)) {
