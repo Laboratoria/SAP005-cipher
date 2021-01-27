fe185b5fbfd6698771dd42e55dbc369b8aa912ef
saori m <trhvmn.abcn@gmail.com>
2021-01-27 20:12:40 -0300

    Add cipher code to index.js and remove module import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import cipher from './cipher.js';
+// import cipher from './cipher.js';
 
-function code(event) {
-	cipher.output = cipher.input.replaceAll(/[A-Z]/gi, cipher.formula);
-	event.preventDefault();
+document.querySelector("#submit").onclick = function(event) {
+	let inputText = document.querySelector("#input").value;
+	let offsetNum = Number(document.querySelector("#offset").value);
+
+	function formula(match) {
+		let outputCodePoint = match.codePointAt(0) + offsetNum;
+
+		if (
+			(/[A-Z]/.test(match) && outputCodePoint < 65) ||
+			(/[a-z]/.test(match) && outputCodePoint < 97)) {
+			outputCodePoint += 26;
+		}
+
+		if (
+			(/[A-Z]/.test(match) && outputCodePoint > 90) ||
+			(/[a-z]/.test(match) && outputCodePoint > 122)) {
+			outputCodePoint -= 26;
+		}
+
+		return String.fromCodePoint(outputCodePoint)
+	}
+
+	document.querySelector("#output").value = inputText.replaceAll(/[A-Z]/gi, formula);
 }
-
-document.forms[0].addEventListener("submit", code);
