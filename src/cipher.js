2b4128292e7cb754742f9be5b2d0fa968dd5ec00
saori m <trhvmn.abcn@gmail.com>
2021-01-28 20:33:26 -0300

    Move cipher function from index.js to cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,46 @@
 const cipher = {
-	"input": document.getElementById("input").innerText,
-	"offset": Number(document.getElementById("offset").innerText),
-	"output": document.getElementById("output").innerText,
+	encode(offset, string) {
+		function formula(match) {
+			let outputCodePoint = match.codePointAt(0) + Number(offset.value);
 
-	formula(match) {
-		let outputCodePoint = match.codePointAt(0) + cipher.offset;
-		if (
-			(/[A-Z]/.test(match) && outputCodePoint < 65) ||
-			(/[a-z]/.test(match) && outputCodePoint < 97)) {
-			outputCodePoint + 26
-		} else if (
-			(/[A-Z]/.test(match) && outputCodePoint > 90) ||
-			(/[a-z]/.test(match) && outputCodePoint > 122)) {
-			outputCodePoint - 26
-		}
-		return String.fromCodePoint(outputCodePoint)
+			if (
+				(/[A-Z]/.test(match) && outputCodePoint < 65) ||
+				(/[a-z]/.test(match) && outputCodePoint < 97)) {
+				outputCodePoint += 26;
+			}
+
+			if (
+				(/[A-Z]/.test(match) && outputCodePoint > 90) ||
+				(/[a-z]/.test(match) && outputCodePoint > 122)) {
+				outputCodePoint -= 26;
+			}
+
+			return String.fromCodePoint(outputCodePoint);
+			}
+
+		string.ncodedTxt.value = string.dcodedTxt.value.replaceAll(/[A-Z]/gi, formula);
+	},
+
+	decode(offset, string) {
+		function formula(match) {
+			let outputCodePoint = match.codePointAt(0) - Number(offset.value);
+
+			if (
+				(/[A-Z]/.test(match) && outputCodePoint < 65) ||
+				(/[a-z]/.test(match) && outputCodePoint < 97)) {
+				outputCodePoint += 26;
+			}
+
+			if (
+				(/[A-Z]/.test(match) && outputCodePoint > 90) ||
+				(/[a-z]/.test(match) && outputCodePoint > 122)) {
+				outputCodePoint -= 26;
+			}
+
+			return String.fromCodePoint(outputCodePoint);
+			}
+
+		string.dcodedTxt.value = string.ncodedTxt.value.replaceAll(/[A-Z]/gi, formula);
 	},
 };
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
-// import cipher from './cipher.js';
+import cipher from './cipher.js';
 
-document.querySelector("#submit").onclick = function(event) {
-	let inputText = document.querySelector("#input").value;
-	let offsetNum = Number(document.querySelector("#offset").value);
-
-	function formula(match) {
-		let outputCodePoint = match.codePointAt(0) + offsetNum;
-
-		if (
-			(/[A-Z]/.test(match) && outputCodePoint < 65) ||
-			(/[a-z]/.test(match) && outputCodePoint < 97)) {
-			outputCodePoint += 26;
-		}
-
-		if (
-			(/[A-Z]/.test(match) && outputCodePoint > 90) ||
-			(/[a-z]/.test(match) && outputCodePoint > 122)) {
-			outputCodePoint -= 26;
-		}
-
-		return String.fromCodePoint(outputCodePoint)
-	}
-
-	document.querySelector("#output").value = inputText.replaceAll(/[A-Z]/gi, formula);
+let textAreas = {
+	dcodedTxt: document.querySelector("#dcodedTxt"),
+	ncodedTxt: document.querySelector("#ncodedTxt"),
 }
+let offsetNum = document.querySelector("#offsetNum");
+let encodeBtn = document.querySelector("#encodeBtn");
+let decodeBtn = document.querySelector("#decodeBtn");
+
+encodeBtn.addEventListener("click", function(){cipher.encode(offsetNum, textAreas)});
+decodeBtn.addEventListener("click", function(){cipher.decode(offsetNum, textAreas)});
