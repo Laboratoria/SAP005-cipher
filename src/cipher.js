035ee48fd0072edb379d7410bc46afc4f21dbab9
Lu-Pereira <luci.lv14@gmail.com>
2021-04-11 00:30:37 -0300

    Alteração no nome das classes e ids para inglês.

--- src/cipher.js
+++ src/cipher.js
@@ -1,36 +1,36 @@
 const cipher = {
 
 
-  encode: (offset, mensagem) => {
+  encode: (offset, message) => {
     let cifrar = '';
-    if (mensagem == null || mensagem == "" || offset == null || offset == "") { // Criação de um TypeError para a condição de null ou vazio apresentara a messagem.
+    if (message == null || message == "" || offset == null || offset == "") {
       throw new TypeError("E necessario inserir chave e texto, por favor revise os campos", "cipher.js");
     } else {
-      for (let i = 0; i < mensagem.length; i++) {
-        if ((mensagem.charCodeAt(i) >= 65) && (mensagem.charCodeAt(i) <= 90)) { // Do A ao Z, de acordo com a posição do ASCII.
-          cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 65 + offset) % 26) + 65); // Utilizando o String.fromCharCode para converter o  codigo ASCII
-        } else if ((mensagem.charCodeAt(i) >= 97) && (mensagem.charCodeAt(i) <= 122)) { // Do A ao Z minusculo, de acordo com a posição do ASCII.
-          cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 97 + offset) % 26) + 97);
-        } else if ((mensagem.charCodeAt(i) >= 32) && (mensagem.charCodeAt(i) <= 64)) { // Do espaço, numeros e simbolos, de acordo com a posição do ASCII.
-          cifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 32 + offset) % 33) + 32);
+      for (let i = 0; i < message.length; i++) {
+        if ((message.charCodeAt(i) >= 65) && (message.charCodeAt(i) <= 90)) {
+          cifrar += String.fromCharCode(((message.charCodeAt(i) - 65 + offset) % 26) + 65);
+        } else if ((message.charCodeAt(i) >= 97) && (message.charCodeAt(i) <= 122)) {
+          cifrar += String.fromCharCode(((message.charCodeAt(i) - 97 + offset) % 26) + 97);
+        } else if ((message.charCodeAt(i) >= 32) && (message.charCodeAt(i) <= 64)) {
+          cifrar += String.fromCharCode(((message.charCodeAt(i) - 32 + offset) % 33) + 32);
         }
       }
       return cifrar;
     }
   },
 
-  decode: (offset, mensagem) => {
+  decode: (offset, message) => {
     let decifrar = '';
-    if (mensagem == null || mensagem == "" || offset == null || offset == "") {
+    if (message == null || message == "" || offset == null || offset == "") {
       throw new TypeError("E necessario inserir chave e texto, por favor revise os campos", "cipher.js");
     } else {
-      for (let i = 0; i < mensagem.length; i++) {
-        if ((mensagem.charCodeAt(i) >= 65) && (mensagem.charCodeAt(i) <= 90)) {
-          decifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 90 - offset) % 26) + 90);
-        } else if ((mensagem.charCodeAt(i) >= 97) && (mensagem.charCodeAt(i) <= 122)) {
-          decifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 122 - offset) % 26) + 122);
-        } else if ((mensagem.charCodeAt(i) >= 32) && (mensagem.charCodeAt(i) <= 64)) {
-          decifrar += String.fromCharCode(((mensagem.charCodeAt(i) - 64 - offset) % 33) + 64);
+      for (let i = 0; i < message.length; i++) {
+        if ((message.charCodeAt(i) >= 65) && (message.charCodeAt(i) <= 90)) {
+          decifrar += String.fromCharCode(((message.charCodeAt(i) - 90 - offset) % 26) + 90);
+        } else if ((message.charCodeAt(i) >= 97) && (message.charCodeAt(i) <= 122)) {
+          decifrar += String.fromCharCode(((message.charCodeAt(i) - 122 - offset) % 26) + 122);
+        } else if ((message.charCodeAt(i) >= 32) && (message.charCodeAt(i) <= 64)) {
+          decifrar += String.fromCharCode(((message.charCodeAt(i) - 64 - offset) % 33) + 64);
         }
       }
       return decifrar;
